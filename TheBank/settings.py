35783e1e5bb623eab00d85ba7930289c69afbd7d
jaineshpancholi <jaineshpancholi1@gmail.com>
2021-06-17 14:32:51 +0530

    17/06/21

--- TheBank/settings.py
+++ TheBank/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'xmc1j(zx29bz72l^_zyn0^b)q)al*wp#s4nk2j#^uett0k^tcp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'banksystem7.herokuapp.com']
 
 
 # Application definition
