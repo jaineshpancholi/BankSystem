07fabfd40ea96ecf9780d6f14735c18c6928c60c
jaineshpancholi <jaineshpancholi1@gmail.com>
2021-06-17 12:40:33 +0530

    17/06/2021

--- TheBank/urls.py
+++ TheBank/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from Bank.views import *
 from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +29,7 @@ urlpatterns = [
     path('maketransaction/', MakeTransaction, name="mtransaction"),
     path('transferto/<int:uid>/', TransferTo, name="transfer"),
     path('history/', history, name="history"),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
