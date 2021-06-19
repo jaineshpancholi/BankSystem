8bd9e7384c3eb396213dc93224ed1700eb9fa39a
jaineshpancholi <jaineshpancholi1@gmail.com>
2021-06-20 00:36:11 +0530

    a

--- TheBank/urls.py
+++ TheBank/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('history/', history, name="history"),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),  
 ]
