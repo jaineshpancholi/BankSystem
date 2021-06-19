f79cd8ed5b337a29fe01d2bb55154e9f4e9fb791
jaineshpancholi <jaineshpancholi1@gmail.com>
2021-06-19 12:23:07 +0530

    19/06/21

--- TheBank/settings.py
+++ TheBank/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
