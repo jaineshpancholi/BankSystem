9d4ca1ae26a9133e61daf0ee4e67380fd345df88
jaineshpancholi <jaineshpancholi1@gmail.com>
2021-06-19 15:46:00 +0530

    setting

--- TheBank/settings.py
+++ TheBank/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'xmc1j(zx29bz72l^_zyn0^b)q)al*wp#s4nk2j#^uett0k^tcp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'banksystem7.herokuapp.com']
 
